441de0c6a39b42ec37390dfabb6d0fc0929c46a7
Player1os <player1os@gmail.com>
2012-11-12 07:13:06 +0100

    Some basic Object Structure ... i mean super basic srsly this is just the beginning MUAHAHAHAHAHA

---
+++ src/aceofspades/CardSet.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aceofspades;
+
+/**
+ * 
+ * @author Player1os
+ */
+public class CardSet {
+
+}
